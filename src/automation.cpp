13e4be92e6df4605531c6dae3c0dd126f34a8162
Alexandre B <alexandre.bobkov@gmail.com>
2023-05-01 00:03:04 -0400

    led

--- src/automation.cpp
+++ src/automation.cpp
@@ -3,5 +3,25 @@
 Automation::Automation() {
 
 }
+void Automation::init() {
+
+}
 
 void Automation::SetOutput(int pin, boolean level) {}
+
+// Dummy task blinking built-in LED
+void Automation::IdleTaskCode (void * parameters) {
+    Serial.print("Task 0 running on core # ");
+    Serial.println(xPortGetCoreID());
+
+    for (;;) {
+      digitalWrite(output_1, HIGH);
+      vTaskDelay(250);
+      digitalWrite(output_1, LOW);
+      vTaskDelay(250);
+      digitalWrite(output_1, HIGH);
+      vTaskDelay(250);
+      digitalWrite(output_1, LOW);
+      vTaskDelay(1500);                
+    }
+}
